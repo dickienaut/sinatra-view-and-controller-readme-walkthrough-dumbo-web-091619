fbb1df0fdd93da2dd5fbee6e24f97a947ebd5162
lyle dickie <lyle.e.dickie@gmail.com>
2019-10-10 13:14:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
   get '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    binding.pry
     erb :reverse
   end
 
